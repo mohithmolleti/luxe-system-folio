037e778833a086edab9c8c2e9b30458838392a61
Mohith Kumar Molleti <mohithmolleti369@gmail.com>
2026-05-28 21:18:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-      },
-    },
   nitro: {
     preset: "vercel",
     },
